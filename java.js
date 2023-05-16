e6781179fe3d72cc5507b5cb4128873c6074cf43
PieroMejia <pmejia@unsa.edu.pe>
2023-05-16 14:53:06 -0500

    2

--- java.js
+++ java.js
@@ -1,1 +1,13 @@
-console.log("hola")
+console.log("hola\n");
+
+
+console.log(arrayGenerator(5,10,15));
+
+function arrayGenerator(n,min,max){
+    let a = [];
+    for(let x=0; x<n; x++){
+        a.push(Math.ceil(Math.random()*(max-min)+min));
+
+    }
+    return a;
+}
